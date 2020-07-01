0059c06a47df732bd5e394e78114c4648188c543
Wilson Tseng <p780729@hotmail.com>
2020-07-01 15:49:27 +0800

    生成測試假資料

---
+++ seeders/20200701072415-products-seed-file.js
@@ -0,0 +1,23 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Products', 
+      Array.from({length: 10}).map((item, index) =>
+      ({
+        id: index + 1,
+        name: faker.commerce.productName(),
+        description: faker.commerce.product()+'/'+faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.image.imageUrl(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })
+    ), {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Products', null, {})
+  }
+};
