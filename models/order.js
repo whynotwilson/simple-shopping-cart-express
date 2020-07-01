521a5cd901e17d5eb108bfa48e66984363fb3a89
Wilson Tseng <p780729@hotmail.com>
2020-07-01 15:01:01 +0800

    定義 Model 間關係

--- models/order.js
+++ models/order.js
@@ -10,7 +10,15 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Order.belongsToMany(models.Product, { 
+        as: 'items', 
+        through: { 
+          model: models.OrderItem, unique: false 
+        }, 
+        foreignKey: 'OrderId'
+      });
+      Order.belongsTo(models.User)
+      Order.hasMany(models.Payment)
     }
   };
   Order.init({
